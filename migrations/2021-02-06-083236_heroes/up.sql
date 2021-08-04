5acb8cfb5b1f2a6f0e68d5e1519387efa7c0cc4c
Brian Tacderan <briantacderan@gmail.com>
2021-08-04 03:39:55 -0700

    Initial commit

---
+++ migrations/2021-02-06-083236_heroes/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE heroes (
+    id SERIAL PRIMARY KEY,
+    fantasy_name VARCHAR NOT NULL,
+    real_name VARCHAR NULL,
+    spotted_photo TEXT NOT NULL,
+    strength_level INT NOT NULL DEFAULT 0
+);
